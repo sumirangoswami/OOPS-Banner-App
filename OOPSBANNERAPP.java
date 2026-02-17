86f5e30c37a69148ba344ee2d415909a73588223
sumirangoswami <dayalsumiran2006@gmail.com>
2026-02-17 08:42:58 +0530

    Initial commit

---
+++ OOPSBANNERAPP.java
@@ -0,0 +1,15 @@
+/**
+* OOPSBannerApp UC1 - OOPS Banner Display Application 
+*
+*This class demonstrates a simple java application that displays the object
+*Oriented programming System OOPS acronym to the console 
+*
+*@sumirangoswami Developer
+@version 1.0
+*/
+public class OOPSBannerApp{
+	
+	public static void main(String[] args){
+     System.out.println("OOPS");
+	}
+}
