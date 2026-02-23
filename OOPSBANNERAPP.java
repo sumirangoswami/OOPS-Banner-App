730bba094bda775c18fa02e013ad5a5618113775
sumirangoswami <dayalsumiran2006@gmail.com>
2026-02-23 09:32:24 +0530

    fixed errors in banner app

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,15 +1,16 @@
-/**
-* OOPSBannerApp UC1 - OOPS Banner Display Application 
-*
-*This class demonstrates a simple java application that displays the object
-*Oriented programming System OOPS acronym to the console 
-*
-*@sumirangoswami Developer
-@version 1.0
-*/
-public class OOPSBannerApp{
-	
-	public static void main(String[] args){
-     System.out.println("OOPS");
-	}
+public class OOPSBannerAPP
+{
+		public static void main(String args[])
+		
+		{
+
+		System.out.println("	***		"+	"	 ***	"+"	 *****  "+"	 ****** ");
+		System.out.println("**       ** "+  " **      ** " + "**   ** "+" **   ");
+		System.out.println("**       ** "+  " **      **" + " **   ** "+"   **   ");
+		System.out.println("**       ** "+  " **      **" + " ******  "+"    **** ");
+		System.out.println("**       ** "+  " **      **" + " **      "+"       **");
+		System.out.println("**       ** "+  " **      **" + " **      "+"       **");
+		System.out.println("    ***     "+  "     ***   " + "  **     "+"  *****   ");
+		}
 }
+
