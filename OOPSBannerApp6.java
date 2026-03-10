bb12a6242abc8045b7581be3fe07fd25307b2ffc
sumirangoswami <dayalsumiran2006@gmail.com>
2026-03-10 09:11:41 +0530

    Coded to Print OOPS to Console

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,56 @@
+public class OOPSBannerApp6 {
+	public static String[] getOPattern() {
+		return new String[] {
+			"   *   ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			"   *   ",
+		};
+	}
+	public static String[] getQPattern() {
+		return new String[] {
+			"   *   ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			" *   * ",
+			"   *   ",
+		};
+	}
+	public static String[] getPPattern() {
+		return new String[] {
+			"   **   ",
+			" *      * ",
+			" *      * ",
+			" *      * ",
+			" ***    ",
+			" **         ",
+			" **         ",
+		};
+	}
+	public static String[] getSPattern() {
+		return new String[] {
+			"   **   ",
+			" *        ",
+			"  *       ",
+			"    ***   ",
+			"       *  ",
+			"       *  ",
+			"   **   ",
+		};
+	}
+	public static void main(String[] args) {
+		String[] oPattern = getOPattern();
+		String[] qPattern = getQPattern();
+		String[] pPattern = getPPattern();
+		String[] sPattern = getSPattern();
+		
+		for (int i = 0; i < oPattern.length; i++) {
+			System.out.println(oPattern[i]+ " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+		}
+	}
+}
